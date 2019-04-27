66ecd04451231184f95cdadc68c553c075586bf5
Long Phan <lnp26@case.edu>
2019-04-27 16:59:39 -0400

    relative (but dont change countdown) with remove top white space

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Logo from './components/Logo.js';
 import Content from "./components/Content.js";
 import headerBackground from "./Assets/backgrounds/Header_Section_Background.svg";
 import './App.css';
+import Countdown from './components/Countdown.js';
 
 class App extends Component{
   constructor(props){
@@ -13,6 +14,7 @@ class App extends Component{
     return (
       <div className = "App">
         <section className = "introduction" style={{ backgroundImage: `url(${headerBackground})` }}>
+          <Countdown />
           <Content />
         </section>
       </div>
