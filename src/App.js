737bfe6deb186b4a8d86676404cc084777efa217
Long Phan <lnp26@case.edu>
2019-04-28 15:39:47 -0400

    Update responsive for mobile phone (header and intro)

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ class App extends Component{
 
   render() {
     return (
-      <div className = "App">
-        <section className = "introduction" style={{ backgroundImage: `url(${headerBackground})` }}>
+      <div className = "App" >
+        <section className = "introduction"  style={{ backgroundImage: `url(${headerBackground})` }}>
           <Countdown HackDate = 'Feb 15, 2020 21:00:00'/>
           <Content />
         </section>
