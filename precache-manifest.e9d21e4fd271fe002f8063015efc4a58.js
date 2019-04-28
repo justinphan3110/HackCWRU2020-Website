eb740f71f22ad8fa21c15d093e2c8b359a621046
Long Phan <lnp26@case.edu>
2019-04-28 15:55:05 -0400

    Updates

---
+++ precache-manifest.e9d21e4fd271fe002f8063015efc4a58.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74ee3ae8f6ecf99dbf2ec5570beeb6fc",
+    "url": "/HackCWRU2020-Website/index.html"
+  },
+  {
+    "revision": "a8267d051f6eb6fae51f",
+    "url": "/HackCWRU2020-Website/static/css/main.60fe4806.chunk.css"
+  },
+  {
+    "revision": "ec17dd9d751765d291bb",
+    "url": "/HackCWRU2020-Website/static/js/2.609907aa.chunk.js"
+  },
+  {
+    "revision": "a8267d051f6eb6fae51f",
+    "url": "/HackCWRU2020-Website/static/js/main.b1cd12d0.chunk.js"
+  },
+  {
+    "revision": "cdb5e08686604baf7b40",
+    "url": "/HackCWRU2020-Website/static/js/runtime~main.56949ac0.js"
+  },
+  {
+    "revision": "eb5482ff168503ba7c3a36af9fede6ef",
+    "url": "/HackCWRU2020-Website/static/media/Header_Section_Background.eb5482ff.svg"
+  },
+  {
+    "revision": "51a1511ae4c7f56ebedb032e98d54a20",
+    "url": "/HackCWRU2020-Website/static/media/Pixelated Spartron.51a1511a.svg"
+  },
+  {
+    "revision": "935e5f03de9f4321742b94e16c292d59",
+    "url": "/HackCWRU2020-Website/static/media/Subtitle 2020.935e5f03.svg"
+  },
+  {
+    "revision": "5b4295e69424ab68d2b19d3c0ac1adbb",
+    "url": "/HackCWRU2020-Website/static/media/Title.5b4295e6.svg"
+  },
+  {
+    "revision": "4e9a195e88e19adc94992d05f63db8ac",
+    "url": "/HackCWRU2020-Website/static/media/prstartk.4e9a195e.ttf"
+  },
+  {
+    "revision": "48b588ca115eb80097f897d503459def",
+    "url": "/HackCWRU2020-Website/static/media/register button up.48b588ca.svg"
+  }
+]);
